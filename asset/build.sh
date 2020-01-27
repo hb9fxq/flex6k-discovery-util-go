2e4c30b4b8ec18ff200844aecad92dc70cf2b8f3
Frank Werner-Krippendorf <f102@Franks-MBP.home-hosts.superlogical.ch>
2020-01-27 22:52:06 +0100

    build.sh

--- asset/build.sh
+++ asset/build.sh
@@ -2,17 +2,17 @@
 cd ..
 
 # Linux
-env GOOS=linux GOARCH=amd64 go build -o ../../../../bin/flex6k-discovery-util-go/linux64/flexi
-env GOOS=linux GOARCH=386 go build -o ../../../../bin/flex6k-discovery-util-go/linux32/flexi
+env GOOS=linux GOARCH=amd64 go build -o ../../../../bin/flex6k-discovery-util-go-build/linux64/flexi
+env GOOS=linux GOARCH=386 go build -o ../../../../bin/flex6k-discovery-util-go-build/linux32/flexi
 
 # Raspi
-env GOOS=linux GOARCH=arm GOARM=5 go build -o ../../../../bin/flex6k-discovery-util-go/raspberryPi/flexi
+env GOOS=linux GOARCH=arm GOARM=5 go build -o ../../../../bin/flex6k-discovery-util-go-build/raspberryPi/flexi
 
 # Windows
-env GOOS=windows GOARCH=amd64 go build -o ../../../../bin/flex6k-discovery-util-go/Win64/flexi.exe
-env GOOS=windows GOARCH=386 go build -o ../../../../bin/flex6k-discovery-util-go/Win32/flexi.exe
+env GOOS=windows GOARCH=amd64 go build -o ../../../../bin/flex6k-discovery-util-go-build/Win64/flexi.exe
+env GOOS=windows GOARCH=386 go build -o ../../../../bin/flex6k-discovery-util-go-build/Win32/flexi.exe
 
 
 # pfsense
-env GOOS=freebsd GOARCH=amd64 go build -o ../../../../bin/flex6k-discovery-util-go/pfSense64/flexi
-env GOOS=freebsd GOARCH=386 go build -o ../../../../bin/flex6k-discovery-util-go/pfSense32/flexi
+env GOOS=freebsd GOARCH=amd64 go build -o ../../../../bin/flex6k-discovery-util-go-build/pfSense64/flexi
+env GOOS=freebsd GOARCH=386 go build -o ../../../../bin/flex6k-discovery-util-go-build/pfSense32/flexi
